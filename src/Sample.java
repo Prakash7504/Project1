e8c30d38b4c8c044a55bd31205761e8cc23941ad
Prakash7504 <160813491+Prakash7504@users.noreply.github.com>
2024-03-31 14:20:04 +0530

    FirstCommit

---
+++ src/Sample.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://mpsc.gov.in/");
+		driver.switchTo().newWindow(WindowType.TAB);
+		String p=driver.getWindowHandle();
+		driver.switchTo().window(p);
+		driver.get("https://www.atlassian.com/jira");
+		System.out.println(driver.getTitle());
+	}
+
+}
